bf588bcd28e176ad756ef82d9ebd4e54eb1a5d3d
Carsten <carsten@kopis.de>
2015-05-22 12:36:22 +0200

    Switch from nosetests to manage.py test.

---
+++ pythonproxy/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+class QuestionMethodTests(TestCase):
+
+    def test_adding_numbers_is_working(self):
+        """
+	First testcase, just a dummy.
+        """
+        self.assertEqual(2+3+4, 9)
+
